d1789dafaff6e0e6b43f5090d2108d9e4937853a
Malin Persson <malin.persson02@hotmail.com>
2026-02-08 15:47:28 +0100

    Altered filter function to also combine exchange rate data and weather data for scoring

--- utils/helpers/filter.js
+++ utils/helpers/filter.js
@@ -1,17 +1,24 @@
 import exchangeRate from "../api/exchangeRates.js";
 import { getWeatherData } from "../api/weather.js";
 
+// Filters and combines exchange rate and weather data based on user input
 export const filterResults = async (userInput) => {
     try {
-        const exchangeRates = await exchangeRate(userInput.currency);
+        const exchangeRates = await exchangeRate(userInput.currency.toLowerCase());
         const weather = await getWeatherData(userInput.region);
-        const countriesInRegion = new Set(weather.map(item => item.country));
 
-        const filteredRates = exchangeRates.filter(item =>
-            countriesInRegion.has(item.country)
-        );
+        const combined = weather.map(weatherItem => {
+            const rateItem = exchangeRates.find(rate => rate.country === weatherItem.country);
+            
+            return {
+                ...weatherItem,
+                ...rateItem
+            };
 
-        return filteredRates;
+        // Removes countries that don't have a matching currency in the exchange rates data
+        }).filter(item => item.currency);
+
+        return combined;
     } catch (error) {
         console.log(error);
         return [];
